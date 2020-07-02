7c6b1cac8ef863a669c927294c5a2b8b068e123c
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-07-02 12:35:41 +0530

    chore: add query functions to whitelist
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- erpnext/manufacturing/doctype/bom/bom.py
+++ erpnext/manufacturing/doctype/bom/bom.py
@@ -909,6 +909,7 @@ def get_bom_diff(bom1, bom2):
 
 	return out
 
+@frappe.whitelist()
 def item_query(doctype, txt, searchfield, start, page_len, filters):
 	meta = frappe.get_meta("Item", cached=True)
 	searchfields = meta.get_search_fields()
