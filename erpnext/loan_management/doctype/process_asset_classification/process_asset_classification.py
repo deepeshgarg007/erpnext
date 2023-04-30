c672537cd0f7df8b9a4425be67d1dd105e4c5c66
Deepesh Garg <deepeshgarg6@gmail.com>
2023-04-30 17:20:34 +0530

    feat: Process loan asset classification

---
+++ erpnext/loan_management/doctype/process_asset_classification/process_asset_classification.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.utils import getdate
+
+
+class ProcessAssetClassification(Document):
+	def on_submit(self):
+		from erpnext.loan_management.doctype.loan.loan import update_days_past_due_in_loans
+
+		update_days_past_due_in_loans(
+			posting_date=self.posting_date, loan_type=self.loan_type, loan_name=self.loan
+		)
+
+
+def create_process_asset_classification(posting_date=None, loan_type=None, loan=None):
+	posting_date = posting_date or getdate()
+	asset_classification = frappe.new_doc("Process Asset Classification")
+	asset_classification.posting_date = posting_date
+	asset_classification.loan_type = loan_type
+	asset_classification.loan = loan
+	asset_classification.submit()
